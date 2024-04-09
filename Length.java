a819b42e44b009c722f9dc2de3fbfdb5d33fcfb8
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:17:10 +0545

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,9 @@
+public class Length{
+    public static void main(String[] args) {
+        String s1 = "Abhishek";
+        String s2 = "sardar";
+
+        System.out.println(s1.length());
+        System.out.println(s2.length());
+    }
+}
