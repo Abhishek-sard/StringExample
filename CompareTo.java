c065f5d9f4293cd902914bc5847ca76079238cad
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:16:05 +0545

    Add files via upload

---
+++ CompareTo.java
@@ -0,0 +1,13 @@
+public class CompareTo {
+    public static void main(String[] args) {
+        String s1 = "Hello world";
+        String s2 = "hello";
+        String s3 = "Hello world";
+        String s4 = "how";
+
+        System.out.println(s1.equals(s2));//false
+        System.out.println(s1.equals(s3));//true
+        System.out.println(s2.equals(s4));//false
+    }
+}
+
