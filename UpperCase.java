fa9fe5de86cbd797a1fb6b667eccb2a62152219d
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:18:16 +0545

    Add files via upload

---
+++ UpperCase.java
@@ -0,0 +1,7 @@
+public class UpperCase {
+    public static void main(String[]args){
+        String s1 = "where are you guys now?";
+        String upr= s1.toUpperCase();
+        System.out.println(upr);
+    }
+}
