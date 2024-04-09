f9dd44698c808250be2b66bc4b44008591f614b1
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:17:57 +0545

    Add files via upload

---
+++ TrimString.java
@@ -0,0 +1,7 @@
+public class TrimString {
+    public static void main(String[] args) {
+        String s1 = "How old are you";
+        System.out.println(s1+"Where are you guys");//without trim method
+        System.out.println(s1.trim()+"who are you guys");//with trim method
+    }
+}
