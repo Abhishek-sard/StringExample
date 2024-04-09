bcfb4b0728f3b4e387545d734420d9b2040acc84
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:17:34 +0545

    Add files via upload

---
+++ LowerCase.java
@@ -0,0 +1,7 @@
+public class LowerCase {
+    public static void main(String[]args){
+        String s1 = "HELLO HOW ARE YOU GUYS?";
+        String slr= s1.toLowerCase();
+        System.out.println(slr);
+    }
+}
