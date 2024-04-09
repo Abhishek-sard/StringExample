1405b7043ff5fda5fe8d9bbdcfc457372230d6ac
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-09 12:16:48 +0545

    Add files via upload

---
+++ EmptyString.java
@@ -0,0 +1,8 @@
+public class EmptyString {
+    public static void main(String[] args) {
+        String s1= "";
+        String s2= "Hello world";
+        System.out.println(s1.isEmpty());//true
+        System.out.println(s2.isEmpty());//false
+    }
+}
